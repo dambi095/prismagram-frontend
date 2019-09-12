f5eee939ba06704f5e84a61b9b695f541fbe1e39
dambya <qwerty6789011@gmail.com>
2019-09-12 23:58:09 +0900

    인스타그램 배경색 설정

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -2,9 +2,17 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 // styled-components 의 최신 방식으로 만드는 global styles
-export default createGlobalStyle `
+export default createGlobalStyle`
     ${reset};
     * {
         box-sizing: border-box;
+    }
+    body {
+        background-color:${props => props.theme.bgColor};
+        color:${props => props.theme.blackColor};
+    }
+    a {
+        color: ${props => props.theme.blueColor};
+        text-decoration: none;
     }
 `;
